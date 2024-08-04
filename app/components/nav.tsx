c3ca8b17bf8eac4cc94d6936dea6cffe1a055c3a
Lucas Neuhaus <38574361+Neuhaus93@users.noreply.github.com>
2024-08-04 17:37:55 -0300

    Improved authorization UI (#3)
    Co-authored-by: Lucas Neuhaus <lucas.neuhaus@bairesdev.com>

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,3 +1,4 @@
+import { SignOutButton } from "@clerk/remix";
 import { Link } from "@remix-run/react";
 import { LucideIcon } from "lucide-react";
 import { buttonVariants } from "~/components/ui/button";
@@ -23,64 +24,74 @@ export function Nav({ links, isCollapsed }: NavProps) {
   return (
     <nav
       data-collapsed={isCollapsed}
-      className={cn(
-        "sticky group top-0 left-0 h-screen gap-2 px-2 pt-10 flex flex-col",
-      )}
+      className="fixed group left-0 top-0 w-[180px] bottom-0 gap-4 flex py-2 px-2 flex-col justify-between"
     >
-      <h3 className="text-lg mb-4 text-center italic">My Expenses</h3>
-      {links.map((link, index) =>
-        isCollapsed ? (
-          <Tooltip key={index} delayDuration={0}>
-            <TooltipTrigger asChild>
-              <Link
-                to={link.href}
-                className={cn(
-                  buttonVariants({ variant: link.variant, size: "icon" }),
-                  "h-9 w-9",
-                  link.variant === "default" &&
-                    "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white",
+      <div className="flex flex-col gap-2">
+        <h2 className="mb-4 mt-2 italic text-lg pl-3">My Expenses</h2>
+        {links.map((link, index) =>
+          isCollapsed ? (
+            <Tooltip key={index} delayDuration={0}>
+              <TooltipTrigger asChild>
+                <Link
+                  to={link.href}
+                  className={cn(
+                    buttonVariants({ variant: link.variant, size: "icon" }),
+                    "h-9 w-9",
+                    link.variant === "default" &&
+                      "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white",
+                  )}
+                >
+                  <link.icon className="h-4 w-4" />
+                  <span className="sr-only">{link.title}</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right" className="flex items-center gap-4">
+                {link.title}
+                {link.label && (
+                  <span className="ml-auto text-muted-foreground">
+                    {link.label}
+                  </span>
                 )}
-              >
-                <link.icon className="h-4 w-4" />
-                <span className="sr-only">{link.title}</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right" className="flex items-center gap-4">
+              </TooltipContent>
+            </Tooltip>
+          ) : (
+            <Link
+              key={index}
+              to={link.href}
+              className={cn(
+                buttonVariants({ variant: link.variant, size: "sm" }),
+                link.variant === "default" &&
+                  "w-[160px] dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                "justify-start",
+              )}
+            >
+              <link.icon className="mr-2 h-4 w-4" />
               {link.title}
               {link.label && (
-                <span className="ml-auto text-muted-foreground">
+                <span
+                  className={cn(
+                    "ml-auto",
+                    link.variant === "default" &&
+                      "text-background dark:text-white",
+                  )}
+                >
                   {link.label}
                 </span>
               )}
-            </TooltipContent>
-          </Tooltip>
-        ) : (
-          <Link
-            key={index}
-            to={link.href}
-            className={cn(
-              buttonVariants({ variant: link.variant, size: "sm" }),
-              link.variant === "default" &&
-                "w-[160px] dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
-              "justify-start",
-            )}
-          >
-            <link.icon className="mr-2 h-4 w-4" />
-            {link.title}
-            {link.label && (
-              <span
-                className={cn(
-                  "ml-auto",
-                  link.variant === "default" &&
-                    "text-background dark:text-white",
-                )}
-              >
-                {link.label}
-              </span>
-            )}
-          </Link>
-        ),
-      )}
+            </Link>
+          ),
+        )}
+      </div>
+      <SignOutButton>
+        <button
+          className={cn(
+            buttonVariants({ variant: "outline", size: "icon" }),
+            "mb-4 w-full text-start",
+          )}
+        >
+          Logout
+        </button>
+      </SignOutButton>
     </nav>
   );
 }
