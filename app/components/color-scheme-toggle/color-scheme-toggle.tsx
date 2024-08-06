5f682a7790555faa15c23b71ee4e2ed5f7bb7159
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2024-08-06 17:16:28 -0300

    Adding header with theme toggling

---
+++ app/components/color-scheme-toggle/color-scheme-toggle.tsx
@@ -0,0 +1,29 @@
+import classes from "./color-scheme-toggle.module.css";
+import {
+  ActionIcon,
+  useMantineColorScheme,
+  useComputedColorScheme,
+} from "@mantine/core";
+import { IconSun, IconMoon } from "@tabler/icons-react";
+import cx from "clsx";
+
+export function ColorSchemeToggle() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+
+  return (
+    <ActionIcon
+      onClick={() =>
+        setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+      }
+      variant="default"
+      size="lg"
+      aria-label="Toggle color scheme"
+    >
+      <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+      <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+    </ActionIcon>
+  );
+}
