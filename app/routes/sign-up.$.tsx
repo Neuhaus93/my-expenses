031ba7f451a2acd33568f2e701fe6b2384386d91
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2024-09-12 08:46:03 -0300

    Removing remaining clerk mentions

--- app/routes/sign-up.$.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/remix";
-
-export default function SignUpPage() {
-  return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <SignUp />
-    </div>
-  );
-}
