8cc67b60ea313d4a46701476034c1280c31724bd
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2024-09-13 14:11:30 -0300

    Removing Discord from env schema

--- app/env.server.ts
+++ app/env.server.ts
@@ -3,8 +3,6 @@ import { z } from "zod";
 const envSchema = z.object({
   SECRET: z.string(),
   DATABASE_URL: z.string(),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
 });
 
 // eslint-disable-next-line no-undef
